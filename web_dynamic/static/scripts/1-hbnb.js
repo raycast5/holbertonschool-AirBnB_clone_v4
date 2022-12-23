8ad0249c1ae309ec154cb3eeafbb9a81e616f844
raycast5 <ray.j.colon@gmail.com>
2022-12-23 07:40:53 -0400

    did excercise 1

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,15 @@
+$(() => {
+    const amenityDict = {};
+    $(':checkbox').change(function () {
+      if (this.checked) {
+        amenityDict[$(this).data('id')] = $(this).data('name');
+      } else {
+        delete amenityDict[$(this).data('id')];
+      }
+      if ($.isEmptyObject(amenityDict)) {
+        $('.amenities h4').html('&nbsp');
+      } else {
+        $('.amenities h4').text(Object.values(amenityDict).join(', '));
+      }
+    });
+  });
